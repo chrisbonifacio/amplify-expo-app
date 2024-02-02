23acbac20568caf1f294c6432d1c75d8ed3debf6
Chris Bonifacio <christopher.bonifacio@gmail.com>
2024-02-02 09:43:14 -0500

    init repro for datastore storage adapter issue

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "amplifyexpoapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
